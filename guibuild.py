24028b293a0f8d9edaf699a8293c1077fb6929b8
Jonathan Kelly <jkenniskerry@gmail.com>
2024-08-26 10:36:53 +0100

    Added in Notebook Modification

--- guibuild.py
+++ guibuild.py
@@ -1,21 +1,23 @@
 import tkinter as tk 
 import subprocess
+import webbrowser
+import os
 
-def launch_program_1():
+def Monte_Carlo():
     subprocess.Popen(['python', 'MonteCarlo.py'])
 
 
-def launch_program_2():
+def Time_Complexity():
     subprocess.Popen(['python', 'TIMECOMPLEXITYTEST2.py'])
 
-def launch_program_3():
-    subprocess.Popen(['python', 'PercentageDifferenceOfficial.ipynb'])
+def PC_Growth_Notebook():
+    subprocess.Popen(['jupyter', 'notebook', 'PercentageDifferenceOfficial.ipynb'])
 
-def launch_program_4():
+def Simple_Baseline_Refresh():
     subprocess.Popen(['python', 'SimpleBaselineRefresh.py'])
 
 
-def launch_program_5():
+def Complex_Baseline_Refresh():
     subprocess.Popen(['python', 'ComplexBaselineRefresh.py'])
 
 
@@ -23,19 +25,19 @@ root = tk.Tk()
 root.title('Program Launcher')
 root.geometry('300x300')
 
-btn_program_1 = tk.Button(root, text="Monte Carlo Simulation", command=launch_program_1)
+btn_program_1 = tk.Button(root, text="Monte Carlo Simulation", command=Monte_Carlo)
 btn_program_1.pack(pady=10)
 
-btn_program_2 = tk.Button(root, text="Time Complexity", command=launch_program_2)
+btn_program_2 = tk.Button(root, text="Time Complexity", command=Time_Complexity)
 btn_program_2.pack(pady=10)
 
-btn_program_3 = tk.Button(root, text="Percentage Growth from Brute Force", command=launch_program_3)
+btn_program_3 = tk.Button(root, text="Percentage Growth from Brute Force", command=PC_Growth_Notebook)
 btn_program_3.pack(pady=10)
 
-btn_program_4 = tk.Button(root, text="Simple Baseline Refresh", command=launch_program_4)
+btn_program_4 = tk.Button(root, text="Simple Baseline Refresh", command=Simple_Baseline_Refresh)
 btn_program_4.pack(pady=10)
 
-btn_program_5 = tk.Button(root, text="Complex Baseline Refresh", command=launch_program_5)
+btn_program_5 = tk.Button(root, text="Complex Baseline Refresh", command=Complex_Baseline_Refresh)
 btn_program_5.pack(pady=10)
 
 root.mainloop()
